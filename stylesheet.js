93d855a89563ff6cdd6328cedba47df25a1e3b17
unknown <itsrickyp23@gmail.com>
2021-08-17 11:39:37 -0400

    Modified CSS, setting up meal planning page

--- stylesheet.js
+++ stylesheet.js
@@ -47,3 +47,14 @@ cancelBtn.onclick = function () {
   menuBtn.style.pointerEvents = "auto";
   navBar.classList.remove("active");
 };
+//Sticky Navigation
+let nav = document.querySelector("nav");
+let val;
+window.onscroll = function(){
+  if(document.documentElement.scrollTop > 20){
+    nav.classList.add("sticky");
+  }
+  else{
+    nav.classList.remove("sticky");
+  }
+}
