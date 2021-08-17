d3e3e4837ebbad6689f66b3130ed99ca1f3a65b4
Nirath Hussan <silver.burner2@gmail.com>
2021-08-16 23:30:55 -0400

    Finished the navbar responsiveness. I also added a few lines in js that create functional buttons for any users that are on smaller devices to be able to open up the navbar. Try it out yourself!

--- stylesheet.js
+++ stylesheet.js
@@ -32,3 +32,18 @@ function darkMode() {
   favicon.setAttribute("href", "images/logoDarkMode.png");
   console.log("dark");
 }
+
+// Side NavIgation Menu JS Code (for phone viewers)
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+let navBar = document.querySelector(".navbar");
+menuBtn.onclick = function () {
+  menuBtn.style.opacity = "0";
+  menuBtn.style.pointerEvents = "none";
+  navBar.classList.add("active");
+};
+cancelBtn.onclick = function () {
+  menuBtn.style.opacity = "1";
+  menuBtn.style.pointerEvents = "auto";
+  navBar.classList.remove("active");
+};
