682c301ccf068cc3b6052cc658a8715f426ec348
Nirath Hussan <silver.burner2@gmail.com>
2021-08-16 22:32:08 -0400

    Just added in a sort of responsive navbar

--- stylesheet.js
+++ stylesheet.js
@@ -1,30 +1,34 @@
-var theme = document.getElementById('theme');
-var favicon = document.getElementById('favicon');
+var theme = document.getElementById("theme");
+var favicon = document.getElementById("favicon");
 
-if (window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches) {
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: light)").matches
+) {
   lightMode();
 } else {
   darkMode();
 }
 
-window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", (e) => {
-  const newColorScheme = e.matches ? "dark" : "light";
-  if (newColorScheme == "light") {
-    lightMode();
-  }
-  else {
-    darkMode();
-  }
-});
+window
+  .matchMedia("(prefers-color-scheme: dark)")
+  .addEventListener("change", (e) => {
+    const newColorScheme = e.matches ? "dark" : "light";
+    if (newColorScheme == "light") {
+      lightMode();
+    } else {
+      darkMode();
+    }
+  });
 
 function lightMode() {
-  theme.setAttribute('href', 'style.css');
-  favicon.setAttribute('href', 'images/logoLightMode.png');
+  theme.setAttribute("href", "style.css");
+  favicon.setAttribute("href", "images/logoLightMode.png");
   console.log("light");
 }
 
 function darkMode() {
-  theme.setAttribute('href', 'style.css');
-  favicon.setAttribute('href', 'images/logoDarkMode.png');
+  theme.setAttribute("href", "style.css");
+  favicon.setAttribute("href", "images/logoDarkMode.png");
   console.log("dark");
 }
