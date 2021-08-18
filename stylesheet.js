546eadcdf50687b3199e211051280f5fb7a8165d
Nirath Hussan <silver.burner2@gmail.com>
2021-08-17 22:41:00 -0400

    Resolved the navbar overlapping problem

--- stylesheet.js
+++ stylesheet.js
@@ -50,11 +50,10 @@ cancelBtn.onclick = function () {
 //Sticky Navigation
 let nav = document.querySelector("nav");
 let val;
-window.onscroll = function(){
-  if(document.documentElement.scrollTop > 20){
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
     nav.classList.add("sticky");
-  }
-  else{
+  } else {
     nav.classList.remove("sticky");
   }
-}
+};
