e4f797d098858d118f77b45497bd51c0302a6258
unknown <itsrickyp23@gmail.com>
2021-08-19 12:05:15 -0400

    Finalized MVP for demo

--- stylesheet.js
+++ stylesheet.js
@@ -1,6 +1,7 @@
 var theme = document.getElementById("theme");
 var favicon = document.getElementById("favicon");
 var logo = document.getElementById("brain");
+var setDark;
 
 if (
   window.matchMedia &&
@@ -26,12 +27,14 @@ function lightMode() {
   theme.setAttribute("href", "styleLight.css");
   favicon.setAttribute("href", "images/logoLightMode.png");
   console.log("light");
+  setDark = false;
 }
 
 function darkMode() {
-  theme.setAttribute("href", "styleLight.css");
+  theme.setAttribute("href", "styleDark.css");
   favicon.setAttribute("href", "images/logoDarkMode.png");
   console.log("dark");
+  setDark = true;
 }
 
 // Side NavIgation Menu JS Code (for phone viewers)
@@ -53,7 +56,11 @@ let nav = document.querySelector("nav");
 let val;
 window.onscroll = function () {
   if (document.documentElement.scrollTop > 20) {
-    logo.setAttribute("src", "images/logoDarkMode.png");
+    if (setDark == true) {
+      logo.setAttribute("src", "images/logoLightMode.png");
+    } else {
+      logo.setAttribute("src", "images/logoDarkMode.png");
+    }
     nav.classList.add("sticky");
   } else {
     logo.setAttribute("src", "images/logoTheme.png");
